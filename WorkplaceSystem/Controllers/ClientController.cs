6f95fe62f79b5d2d281fc7c0720259e5147440ee
PedroHenrique234 <teixeirapedrohenrique234@gmail.com>
2024-04-26 14:51:28 -0300

    Controller are created and configurated

---
+++ WorkplaceSystem/Controllers/ClientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorkplaceSystem.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
